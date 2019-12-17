24fe03817858e1701954eabff7973be93a16d295
dAVE Inden <davidinden@gmail.com>
2019-12-17 14:48:13 -0800

    setup migrations and seeds for prod env
    Co-authored-by: Mackenzie Weber <mack.webb37@gmail.com>

--- data/production/seeds/004-images.js
+++ data/production/seeds/004-images.js
@@ -1,13 +1,25 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('images').insert([
+    {
+      image_url: 'https://images.pexels.com/photos/102127/pexels-photo-102127.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      art_id: 1
+    },
+    {
+      image_url: 'https://images.pexels.com/photos/2303796/pexels-photo-2303796.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      art_id: 2
+    },
+    {
+      image_url: 'https://images.pexels.com/photos/207666/pexels-photo-207666.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      art_id: 3
+    },
+    {
+      image_url: 'https://images.pexels.com/photos/545470/pexels-photo-545470.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      art_id: 4
+    },
+    {
+      image_url: 'https://images.pexels.com/photos/1646981/pexels-photo-1646981.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      art_id: 5
+    }
+  ]);
 };
