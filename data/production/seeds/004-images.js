71d571cf45221306016f5f0297fe597f53075761
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-12-17 15:39:18 -0800

    Updated readme with ENVs, URL to database, and updated images seeds for development/production
    Co-authored-by: Dave Inden<davidinden@gmail.com>

--- data/production/seeds/004-images.js
+++ data/production/seeds/004-images.js
@@ -20,6 +20,26 @@ exports.seed = function(knex) {
     {
       image_url: 'https://images.pexels.com/photos/1646981/pexels-photo-1646981.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       art_id: 5
+    },
+    {
+      image_url: 'https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=940',
+      art_id: 1
+    },
+    {
+      image_url: 'https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      art_id: 2
+    },
+    {
+      image_url: 'https://images.pexels.com/photos/807598/pexels-photo-807598.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      art_id: 3
+    },
+    {
+      image_url: 'https://images.pexels.com/photos/545470/pexels-photo-545470.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      art_id: 4
+    },
+    {
+      image_url: 'https://images.pexels.com/photos/1646981/pexels-photo-1646981.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      art_id: 5
     }
   ]);
 };
