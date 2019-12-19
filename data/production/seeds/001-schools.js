ea3e744338f23ec042ccfe5f8813c98557f79b9f
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-12-19 08:58:26 -0800

    Another test

--- data/production/seeds/001-schools.js
+++ data/production/seeds/001-schools.js
@@ -3,7 +3,7 @@ exports.seed = function(knex) {
   return knex('schools').insert([
     { 
       school_id: '123abc456def789ghi',
-      school_name: 'West High School',
+      school_name: 'West High School TESTING TO SEE IF THIS IS AFFECTED',
       email: 'test-email@example.com',
       address: '123 West St',
       city: 'Westtown',
