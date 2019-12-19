e4fb945647a2175945b698e7b76ee2bc443e2a29
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-12-19 13:32:54 -0800

    Adding a get message to initial GET
    Co-authored-by: Jason Loomis <jason@jrloomis.com>

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ middleware(app)
 
 server.applyMiddleware({ app, path: '/graphql' });
 
+app.get('/', (req, res) => { 
+    res.send(`<h1>Server running!</h1>`)
+})
+
 app.listen(port, () => {
     const message = `Server listening on ${port}`
     console.log(message)
