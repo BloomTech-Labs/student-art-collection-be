a5153e2c2cd9956d2b00399f8216a733b9e92352
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-12-20 15:23:29 -0800

    Trying to see if we can use playground
    Co-authored-by: Dave Inden <davidinden@gmail.com>
    Co-authored-by: Jason Loomis <jason@jrloomis.com>
    Co-authored-by: Grissobel Payonik <grissobel.castro@gmail.com>
    Co-authored-by: Ian Schwartz <ian.schwartz23@gmail.com>

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ const { ApolloServer} = require('apollo-server-express')
 const typeDefs = require('../graphql/schema')
 const resolvers = require('../graphql/resolvers')
 
-const server = new ApolloServer({typeDefs, resolvers});
+const server = new ApolloServer({typeDefs, resolvers, introspection: true, playground: true});
 
 module.exports = server
