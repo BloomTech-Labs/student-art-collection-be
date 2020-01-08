29d56c96d9675e32e528a80ddecdf81d3626b0a5
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-01-08 13:50:30 -0800

    Forgot to save

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,74 +1,6 @@
 const db = require('../data/dbConfig.js');
 
 const resolvers = {
-<<<<<<< HEAD
-    Query: {
-        allSchools: () => {
-            return db('schools')
-        },
-        school: (parent, {id}) => {
-            return db('schools').where({id}).first()
-        },
-        schoolBySchoolId: (parent, {school_id}) => {
-            return db('schools').where({school_id}).first()   
-        },
-        allCategories: () => {
-            return db('categories')
-        },
-        category: (parent, {id}) => {
-            return db('categories').where({id}).first()
-        },
-        allArts: () => {
-            return db('art')
-        },
-        art: (parent, {id}) => {
-            return db('art').where({id}).first()
-        },
-        artBySchool: (parent, {school_id}) => {
-            return db('art').where({school_id})
-        },
-        artByCategory: (parent, {category}) => {
-            return db('art').where("category", "=", category)
-        },
-        allImages: () => {
-            return db('images')
-        },
-        image: (parent, {id}) => {
-            return db('images').where({id}).first()
-        },
-        imageByArt: (parent, {art_id}) => {
-            return db('images').where({art_id})
-        },
-    },
-    Mutation: {
-        addSchool: async (parent, args) => {
-            const [ id ] =  await db('schools').insert(args, 'id')
-            return db('schools').where('id', id).first()
-        },
-        addArt: async (parent, args) => {
-            const [ id ] = await db('art').insert(args, 'id')            
-            return db('art').where('id', id).first()
-        },
-        addImage: async (parent, args) => {
-            const [ id ] = await db('images').insert(args, 'id')
-            return db('images').where('id', id).first()
-        },
-        updateSchool: async (parent, args) => {
-            await db('schools').where('id', args.id).update(args)
-            return db('schools').where('id', args.id).first()
-        },
-        updateArt: async (parent, args) => {
-            await db('art').where('id', args.id).update(args)
-            return db('art').where('id', args.id).first()
-        },
-        updateImage: async (parent, args) => {
-            await db('images').where('id', args.id).update(args)
-            return db('images').where('id', args.id).first()
-        },
-        deleteSchool: async (parent, args) => {
-            const deletedSchool = await db('schools').where('id', args.id).first()
-            await db('schools').where('id', args.id).del()
-=======
   Query: {
     allSchools: () => {
       return db('schools');
@@ -167,7 +99,6 @@ const resolvers = {
       await db('schools')
         .where('id', args.id)
         .del();
->>>>>>> 4def4ec1a717868619c3c7ecd366d67e7459e120
 
       return deletedSchool;
     },
@@ -189,22 +120,6 @@ const resolvers = {
         .where('id', args.id)
         .del();
 
-<<<<<<< HEAD
-            return deletedImage
-        },
-        contact: async (parent, args) => {
-            const email = await db('schools').where('school_id', '=', args.school_id).select('email')
-            
-        }
-    },
-    Art: {
-        images: async (parent) => {
-            const id = parent.id
-            return db('images').where('art_id', id)
-        }
-    }
-}
-=======
       return deletedImage;
     },
   },
@@ -227,6 +142,5 @@ const resolvers = {
     },
   },
 };
->>>>>>> 4def4ec1a717868619c3c7ecd366d67e7459e120
 
 module.exports = resolvers;
