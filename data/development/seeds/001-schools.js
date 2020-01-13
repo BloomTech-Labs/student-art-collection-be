8f3113e36c35fbe966aaa3188d5762805332d2fa
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-01-13 09:53:41 -0800

    Updated seeds in schools file so it matches the uid in our firebase app, along with the email to test sendgrid.
    Co-authored-by: Dave Inden <davidinden@gmail.com>
    Co-authored-by: Ian Schwartz <ian.schwartz23@gmail.com>
    Co-authored-by: Grissobel Payonk <grissobel.castro@gmail.com>
    Co-authored-by: Jason Loomis <jason@jrloomis.com>
    Co-authored-by: Ami Scott <amiezrascott@gmail.com>

--- data/development/seeds/001-schools.js
+++ data/development/seeds/001-schools.js
@@ -2,9 +2,9 @@
 exports.seed = function(knex) {
   return knex('schools').insert([
     { 
-      school_id: '123abc456def789ghi',
+      school_id: 'KcfQHI6dMwQpQg9yvPB4ctD1gNi1',
       school_name: 'West High School',
-      email: 'test-email@example.com',
+      email: 'studentartco@gmail.com',
       address: '123 West St',
       city: 'Westtown',
       zipcode: '12345'
